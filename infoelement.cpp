21051410c16efcbe2931b2a487c0ae125e2ca0cb
MegaCarpe <36008922+MegaCarpe@users.noreply.github.com>
2018-03-14 13:27:39 +0100

    Delete infoelement.cpp

--- infoelement.cpp
+++
@@ -1,8 +0,0 @@
-#include "infoelement.h"
-
-InfoElement::InfoElement(QString _title, QString _description, QString _creationDate, QString _modificationDate, Status _status, const std::vector<QString> & _tags):
-    title(_title),description(_description),creationDate(_creationDate),modificationDate(_modificationDate),tags(_tags),status(_status)
-{
-
-}
-
